bf1bc0a328c963234a39db83adece13aa5e12d49
lioarce01 <lioarce1@gmail.com>
2024-10-08 20:53:53 -0300

    updated route create project and get users

--- api/controllers/projectController.js
+++ api/controllers/projectController.js
@@ -1,14 +1,26 @@
 import createProject from "../services/projectServices.js";
+import User from "../models/userModel.js";
 
 const create = async (req, res) => {
-  const { name, creator, goal_amount } = req.body;
-  if (!(name && creator && goal_amount))
-    return res.status(400).sen({
+  const { name, creator, goal_amount, owner } = req.body;
+  if (!(name && creator && goal_amount && owner))
+    return res.status(400).send({
       errMessage: "El nombre, el creador y la meta, no deben ser nulos.",
     });
 
-  await createProject(req.body, (err, result) => {
+  await createProject(req.body, async (err, result) => {
     if (err) return res.status(400).send(err);
+
+    try {
+      await User.findByIdAndUpdate(owner, {
+        $push: { projects: result._id },
+      });
+    } catch (updateErr) {
+      return res
+        .status(400)
+        .send({ errMessage: "Error al actualizar el usuario." });
+    }
+
     return res.status(200).send(result);
   });
 };
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,12 +3,16 @@ import {
   createUser,
   getUsers,
   deleteUser,
+  getUserById,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 //TODOS LOS USERS
 router.get("/", getUsers);
+
+//OBTENER USER BY ID
+router.get("/:id", getUserById);
 
 //CREAR USER
 router.post("/create", createUser);
