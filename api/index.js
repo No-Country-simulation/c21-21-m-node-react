519d4dc93aa170e9b39c0af4ff539b35057b2b02
lioarce01 <lioarce1@gmail.com>
2024-10-08 17:29:27 -0300

    user controllers and routes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { connectDb } from "./config/dataBase.js";
 import * as dotenv from "dotenv";
-
-import Project from "./models/projectModel.js";
-import User from "./models/userModel.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const PORT = 3000;
 dotenv.config();
@@ -20,23 +18,5 @@ app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
 });
 
-//rutas de prueba (a eliminar)
-app.post("/create-project", async (req, res) => {
-  try {
-    const project = new Project(req.body);
-    const savedProject = await project.save();
-    res.status(201).json(savedProject);
-  } catch (error) {
-    res.status(500).json({ message: "Error al crear el proyecto", error });
-  }
-});
-
-app.post("/create-user", async (req, res) => {
-  try {
-    const user = new User(req.body);
-    const savedUser = await user.save();
-    res.status(201).json(savedUser);
-  } catch (error) {
-    res.status(500).json({ message: "Error al crear el usuario", error });
-  }
-});
+//routes
+app.use("/users", userRoutes);
