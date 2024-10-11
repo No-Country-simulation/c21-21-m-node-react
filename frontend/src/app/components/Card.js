8607fc63fa76319206285835a12b25759fa0b692
Maria Alarcon <mariagaa993@gmail.com>
2024-10-11 09:37:06 -0300

    creación componente modal. Mostrar modal en las funcionalidades que tiene la card

--- frontend/src/app/components/Card.js
+++ frontend/src/app/components/Card.js
@@ -1,6 +1,16 @@
+import React, { useState } from 'react';
 import Button from "./Button";
+import Modal from "./Modal";
 
 const Card = ({ imgSrc, title, percentage, description }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [titleModal, setTitleModal] = useState("");
+
+    const openModal = (title) => {
+        setTitleModal(title)
+        setIsModalOpen(true)
+    };
+
     return (
         <div className="w-full mx-auto">  
             <div className="bg-white shadow-md rounded-lg border border-gray-200 p-0">
@@ -21,24 +31,35 @@ const Card = ({ imgSrc, title, percentage, description }) => {
                     <p className="font-bold text-sm mb-5">${description} recaudados</p> 
                     <hr className="border-gray-300 mb-4" />
                     <div className="flex space-x-2 mb-2">
-                        <Button className="flex-1 bg-yellow-500 text-white rounded-full 
+                        <Button
+                            onClick={() => openModal("Editar campaña")} 
+                            className="flex-1 bg-yellow-500 text-white rounded-full 
                             px-2 py-1 hover:bg-yellow-600 transition duration-200 text-sm">
                             Editar
                         </Button>
-                        <Button className="flex-1 bg-red-500 text-white rounded-full px-2 
+                        <Button 
+                            onClick={() => openModal("Eliminar campaña")}
+                            className="flex-1 bg-red-500 text-white rounded-full px-2 
                             py-1 hover:bg-red-600 transition duration-200 text-sm">
                             Eliminar
                         </Button>
                     </div>
                     <div className="flex mb-2"> 
-                        <Button className="flex-1 bg-blue-500 text-white rounded-full px-2 
+                        <Button
+                            onClick={() => openModal("Inversores")} 
+                            className="flex-1 bg-blue-500 text-white rounded-full px-2 
                             py-1 hover:bg-blue-600 transition duration-200 text-sm">
                             Ver inversores
                         </Button>
                     </div>
                 </div>
             </div>
-        </div>
+            <Modal
+                isOpen={isModalOpen}        
+                onClose={() => setIsModalOpen(false)}
+                title={titleModal}
+            />
+        </div>  
     );
 };
 
---
+++ frontend/src/app/components/Modal.js
@@ -0,0 +1,19 @@
+const Modal = ({ isOpen, onClose, title, children }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 px-1">
+            <div className="bg-white p-6 rounded-lg shadow-lg relative w-full max-w-md mx-auto">
+                <button onClick={onClose} 
+                    className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 text-2xl font-bold">
+                        &times;
+                </button>
+                <h2 className="text-lg font-bold mb-4">{title}</h2>
+                <hr className="border-gray-300 mb-4" />
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
