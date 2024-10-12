b16820fc3c1de15abdf0e414a90dfbc4c103ef11
tomasfriz <tomyagustinf@gmail.com>
2024-10-12 12:42:45 -0300

    se agrego el logo de la marca

--- frontend/src/app/components/Footer.js
+++ frontend/src/app/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="bg-customBlue text-customLightgray mt-8 box-border">
+        <footer className="bg-customGray text-customWhite mt-8 box-border p-1">
             <div className="container mx-auto text-center px-4 py-2">
                 <p className="text-sm">&copy; 2024 BOOSTUP. Reservados todos los derechos.</p>
             </div>
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,15 +5,15 @@ import backgroundImage from './assets/images/banner.jpg';
 
 const Home = () => {
     return (
-        <div 
-            className="flex flex-col min-h-screen bg-cover" 
-            style={{ backgroundImage: `url(${backgroundImage.src})`, backgroundPosition: '20% 0' }}> 
+        <div
+            className="flex flex-col min-h-screen bg-cover"
+            style={{ backgroundImage: `url(${backgroundImage.src})`, backgroundPosition: '20% 0' }}>
             <Navbar />
             <Container className="flex flex-col justify-center items-center flex-1 text-center">
                 <div className="flex flex-col justify-center items-center text-center h-full">
-                    <h1 className="text-4xl md:text-5xl font-bold mb-6 pt-4 md:pt-20 text-white mt-20">Bienvenido a CrowdStart</h1>
-                    <p className="text-lg md:text-xl font-bold mb-6 text-white">Apoya a nuevas empresas innovadoras y haz realidad sus ideas.</p>
-                    <button className="bg-customGreen text-white font-bold px-8 py-3 rounded-full mt-20">Explorar proyectos</button>
+                    <h1 className="text-4xl md:text-5xl font-bold mb-6 pt-4 md:pt-20 text-white mt-20">Bienvenido a BOOSTUP</h1>
+                    <p className="text-lg md:text-xl font-bold mb-6 text-white">Insentiva a nuevas empresas y haz realidad sus ideas.</p>
+                    <button className="bg-customGreen text-white font-bold px-8 py-3 rounded-full mt-20 hover:text-customGreen hover:bg-customGray hover:border hover:border-customGreen">Explorar proyectos</button>
                 </div>
             </Container>
             <Footer />
