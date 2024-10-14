45e44ee1183a1ecb1ae926169c2376490c340586
Maria Alarcon <mariagaa993@gmail.com>
2024-10-14 17:27:57 -0300

    flujo para hacer la inversión con paypal

--- frontend/src/app/components/investment/PaymentMethod.js
+++ frontend/src/app/components/investment/PaymentMethod.js
@@ -1,12 +1,20 @@
+"use client"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShieldAlt } from '@fortawesome/free-solid-svg-icons';
+import PaypalButton from './PaypalButton';
 
 const PaymentMethod = () => { 
+    const investmentAmount = 10.00;
+    
     return (
         <div className="pt-5">
             <div className="h-px bg-gray-200 my-2 md:my-4"></div>
             <p className="font-bold pt-4 md:pt-6 pb-2 md:pb-4">Método de pago</p>
             <small className="text-gray-600">Por los momentos solo contamos con Paypal.
+                <br/>
+                <strong>Nota:</strong> Si no tienes una cuenta de PayPal, puedes realizar tu pago utilizando tu 
+                    tarjeta de crédito o débito directamente a través de PayPal. Simplemente 
+                    selecciona la opción de pagar con tarjeta al finalizar la compra.
             </small>
             <div className="h-px bg-gray-200 my-5 md:my-7"></div>
             <div className="mb-4 md:mb-6">
@@ -31,13 +39,7 @@ const PaymentMethod = () => {
                     No mostrar mi nombre públicamente en la inversión
                 </label>
             </div>
-
-            <button
-                className="w-full bg-blue-600 text-white font-semibold py-2 rounded-md 
-                    hover:bg-blue-700 transition duration-300"
-            >
-                Pagar con PayPal
-            </button>
+            <PaypalButton investmentAmount={investmentAmount}/>
             <div className="h-px bg-gray-200 my-10 md:my-12"></div>
             <div className="flex">
                 <div className="mr-4"> 
