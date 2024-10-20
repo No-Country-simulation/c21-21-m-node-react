ef37020f9a61b4fb1b14c824c64d0a3e9f48e315
Maria Alarcon <mariagaa993@gmail.com>
2024-10-20 18:53:03 -0300

    vista proyectos dinámica. Filtrado por categoría.

--- frontend/src/app/components/Projects.js
+++ frontend/src/app/components/Projects.js
@@ -1,14 +1,15 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 import ProjectsCard from "./cards/Projects";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import PorAhora from '../../../public/banner.jpg';
+import axios from "axios";
 
 const categories = ["Todos", "Animal", "Tecnología", "Salud", "Ambiental"];
 
 const Projects = () => {
+    const [data, setData] = useState();
     const [selectedCategory, setSelectedCategory] = useState("Todos");
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
@@ -17,11 +18,26 @@ const Projects = () => {
         setDropdownOpen(false); 
     };
 
-    return (
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://run.mocky.io/v3/8ab007b8-214b-458d-99b8-61cb42810e38');
+                setData(response.data.projects);
+            } catch (error) {
+                setError(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const filterByCategory = selectedCategory === "Todos" ? data 
+    : data?.filter(project => project.category === selectedCategory);
+
+  return (
         <>
             <h1 className="text-customH1 pb-6 font-bold text-center">Encuentra proyectos interesantes</h1>
-            <div className="flex items-center rounded-full 
-                overflow-hidden shadow-md mb-4 w-full mx-auto max-w-md lg:max-w-2xl">
+            <div className="flex items-center rounded-full overflow-hidden shadow-md mb-4 w-full mx-auto max-w-md lg:max-w-2xl">
                 <input
                     type="text"
                     placeholder="Buscar por proyecto..."
@@ -35,25 +51,18 @@ const Projects = () => {
                 <div className="md:hidden mb-4">
                     <button
                         onClick={() => setDropdownOpen(!dropdownOpen)}
-                        className="flex justify-between items-center w-full px-4 py-2 
-                            border border-gray-300 rounded-full bg-white shadow-md text-gray-600 
-                            focus:outline-none">
+                        className="flex justify-between items-center w-full px-4 py-2 border border-gray-300 rounded-full bg-white shadow-md text-gray-600 focus:outline-none">
                         <span>{selectedCategory}</span>
                         <FontAwesomeIcon icon={dropdownOpen ? faChevronUp : faChevronDown} />
                     </button>
                     {
                         dropdownOpen && (
-                            <div className="absolute left-0 right-0 mt-2 bg-white border 
-                                border-gray-300 rounded-lg shadow-lg z-10 max-w-sm mx-auto">
+                            <div className="absolute left-0 right-0 mt-2 bg-white border border-gray-300 rounded-lg shadow-lg z-10 max-w-sm mx-auto">
                                 {
                                     categories.map((category) => (
                                         <button
                                             key={category}
-                                            className={`block w-full text-left px-4 py-2 
-                                                hover:bg-gray-100 ${
-                                                selectedCategory === category ? "font-bold text-black" 
-                                                : "text-gray-500"}`
-                                            }
+                                            className={`block w-full text-left px-4 py-2 hover:bg-gray-100 ${selectedCategory === category ? "font-bold text-black" : "text-gray-500"}`}
                                             onClick={() => handleCategory(category)}>
                                             {category}
                                         </button>
@@ -70,10 +79,7 @@ const Projects = () => {
                         categories.map((category) => (
                             <button
                                 key={category}
-                                className={`px-5 py-2 rounded-full font-bold transition duration-300 ${
-                                    selectedCategory === category ? "bg-black text-white"
-                                    : "text-gray-500 hover:bg-gray-100 hover:text-black"
-                                }`}
+                                className={`px-5 py-2 rounded-full font-bold transition duration-300 ${selectedCategory === category ? "bg-black text-white" : "text-gray-500 hover:bg-gray-100 hover:text-black"}`}
                                 onClick={() => handleCategory(category)}>
                                 {category}
                             </button>
@@ -81,15 +87,19 @@ const Projects = () => {
                     }
                 </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-                gap-y-6 gap-x-3 w-full">
-                <Card 
-                    imgSrc={PorAhora}
-                    title='Proyecto'
-                    percentage='80'
-                    isProjectsPage={true}>
-                    <ProjectsCard />
-                </Card>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 gap-x-3 w-full">
+                {
+                    filterByCategory?.map((project, index) => (
+                        <Card 
+                            key={project.id || index}
+                            imgSrc={project.image}
+                            title={project.title}
+                            percentage={project.percentage}
+                            isProjectsPage={true}>
+                            <ProjectsCard project={project} />
+                        </Card>
+                    ))
+                }
             </div>
         </>
     );
--- frontend/src/app/components/cards/Projects.js
+++ frontend/src/app/components/cards/Projects.js
@@ -1,5 +1,5 @@
-const Projects = () => {
-    return (<p className="font-bold text-sm">$20.000 recaudados</p>);
+const Projects = ({ project }) => {
+    return (<p className="font-bold text-sm">${project.raised} recaudados</p>);
 };
 
 export default Projects;
