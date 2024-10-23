7d5d2925f91f13c1a657a86c4dfebfa9e67d45b8
Maria Alarcon <mariagaa993@gmail.com>
2024-10-23 07:11:25 -0300

    proteger ruta dashboard. Solo para personas autenticadas

--- frontend/src/app/(pages)/dashboard/page.js
+++ frontend/src/app/(pages)/dashboard/page.js
@@ -1,16 +1,40 @@
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Navbar from '@/app/components/Navbar';
 import Footer from '@/app/components/Footer';
 import Container from '@/app/components/Container';
 import Dashboard from '@/app/components/Dashboard';
+import LoaderView from '@/app/components/loaders/LoaderView';
+import { useUserContext } from '@/app/contexts/UserContext';
 
 const DashboardPage = () => {
+    const { user } = useUserContext(); 
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!user || !user.token) {
+            router.push('/');
+        }
+    }, [user, router]);
+
     return (
         <>
-            <Navbar />
-            <Container>
-                <Dashboard />
-            </Container>
-            <Footer />
+            {
+                !user?.token ? (
+                    <LoaderView />
+                ) 
+                : (
+                    <>
+                        <Navbar />
+                        <Container>
+                            <Dashboard />
+                        </Container>
+                        <Footer />
+                    </>
+                )
+            }
+           
         </>
     );
 };
