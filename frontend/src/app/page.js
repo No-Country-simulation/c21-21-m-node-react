8da33a6ecc835998fb611e2b074d707295f59c17
tomasfriz <tomyagustinf@gmail.com>
2024-10-24 20:40:05 -0300

    se oscurecio el fondo de la imagen de la pagina principal

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -43,35 +43,37 @@ const Home = () => {
     // refactorizar. Crear componente home.
     return (
         <div
-            className="flex flex-col min-h-screen bg-cover bg-customGray"
-            style={{backgroundImage: `url(${backgroundImage.src})`}}>
-            <Navbar />
-            <Container className="flex flex-col justify-center items-center flex-1 text-center">
-                <div className="flex flex-col justify-center items-center text-center h-full">
-                    <h1 className="text-4xl md:text-5xl font-bold mb-6 pt-4 md:pt-20 text-white mt-20">
-                        Bienvenido a BOOSTUP
-                    </h1>
-                    <p className="text-lg md:text-xl font-bold mb-6 text-white">Incentiva a nuevas 
-                        empresas y haz realidad sus ideas.</p>
-                    <Link href="/projects" passHref>
-                        <Button className="bg-customGreen text-white font-bold px-8 py-3 rounded-full 
-                            mt-20 hover:text-customGreen hover:bg-customGray hover:border-customGray">
-                            Explorar proyectos
-                        </Button>
-                    </Link>
-                </div>
-            </Container>
-            <Footer />
+            className="flex flex-col min-h-screen bg-cover bg-customGray relative"
+            style={{ backgroundImage: `url(${backgroundImage.src})` }}>
+            <div className="absolute inset-0 bg-black bg-opacity-50">
+                <Navbar />
+                <Container className="flex flex-col justify-center items-center flex-1 text-center relative z-10">
+                    <div className="flex flex-col justify-center items-center text-center h-full">
+                        <h1 className="text-4xl md:text-5xl font-bold mb-6 pt-4 md:pt-20 text-white mt-10">
+                            Bienvenido a BOOSTUP
+                        </h1>
+                        <p className="text-lg md:text-xl font-bold mb-6 text-white mt-5 mb-10">
+                            Incentiva a nuevas empresas y haz realidad sus ideas.
+                        </p>
+                        <Link href="/projects" passHref>
+                            <Button className="bg-customGreen text-white font-bold px-8 py-3 rounded-full mt-20 hover:text-customGreen hover:bg-customGray hover:border-customGray">
+                                Explorar proyectos
+                            </Button>
+                        </Link>
+                    </div>
+                </Container>
+                <Footer />
+            </div>
             <Modal
                 isOpen={!!errorMessage}
                 onClose={closeModal}
                 title="Error al ..."
-                size='max-w-md max-h-40'
+                size="max-w-md max-h-40"
                 isError={!!errorMessage}>
                 {errorMessage}
             </Modal>
-            {/*isLoading && <LoaderView />*/}
         </div>
+
     );
 };
 
