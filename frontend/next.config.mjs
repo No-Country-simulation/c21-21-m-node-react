149721fc50b153bd61e010821d1b0eb25a05f9e0
Maria Alarcon <mariagaa993@gmail.com>
2024-10-25 15:49:26 -0300

    se agrega las extensiones para las imagenes y para actualizar un proyecto

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'dummyimage.com',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '4000',
+                pathname: '/uploads/**',
+            },
+        ],
+    },
     async rewrites() {
         return [
             {
@@ -13,6 +27,10 @@ const nextConfig = {
             {
                 source: '/api/create-project',
                 destination: 'http://localhost:4000/projects/createProject', 
+            },
+            {
+                source: '/api/update-project/:id', 
+                destination: 'http://localhost:4000/projects/updateProject/:id', 
             },
         ];
     },
